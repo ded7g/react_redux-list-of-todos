04df14ab893a0e3b0b30e999ffa4e0d229975372
mc704ed <mykyta.churylov@student.tuke.sk>
2025-12-23 13:15:26 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getTodos } from './api';
+import { setTodos } from './features/todos';
 import { Loader, TodoFilter, TodoList, TodoModal } from './components';
 
-export const App = () => (
-  <>
-    <div className="section">
-      <div className="container">
-        <div className="box">
-          <h1 className="title">Todos:</h1>
+export const App = () => {
+  const dispatch = useDispatch();
 
-          <div className="block">
-            <TodoFilter />
-          </div>
+  useEffect(() => {
+    getTodos().then(todos => {
+      dispatch(setTodos(todos));
+    });
+  }, [dispatch]);
 
-          <div className="block">
-            <Loader />
-            <TodoList />
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <div className="box">
+            <h1 className="title">Todos:</h1>
+
+            <div className="block">
+              <TodoFilter />
+            </div>
+
+            <div className="block">
+              <Loader />
+              <TodoList />
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <TodoModal />
-  </>
-);
+      <TodoModal />
+    </>
+  );
+};
